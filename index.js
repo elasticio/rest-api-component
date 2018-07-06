4a02359fa9ec0316a817fd2c1be859fd619843b8
ShkarupaNick <shkarupanick@gmail.com>
2018-07-06 15:19:28 +0300

    attachment refactoring

--- index.js
+++ index.js
@@ -1,5 +1,83 @@
-const jsonata = require('@elastic.io/jsonata-moment');
 const rp = require('request-promise');
+const http = require('http');
+const client = require('elasticio-rest-node')('nick@elastic.io', 'df8b012f-7cad-4dc3-81a2-7b2087291b07');
+const uuidv1 = require('uuid/v1');
+const debug = require('debug')('index');
+const url = require('url');
+const toStream = require('string-to-stream');
+
+const requestOptions = {
+  method: 'get',
+  uri: 'https://httpbin.org/image/png',
+  followRedirect: true,
+  followAllRedirects: true,
+  gzip: true,
+  resolveWithFullResponse: true,
+  simple: false
+};
+
+const msg = {};
+getBinaryString().then(result => addAttachment(msg, uuidv1() + '_' + new Date().getTime(), result.body, result.headers['content-length'], result.headers['content-type']));
+
+function getBinaryString() {
+  return rp('https://httpbin.org/image/png', requestOptions).then(result => {
+    return result;
+  })
+}
+
+function addAttachment(msg, name, body, contentLength, contentType) {
+  return getUrls().then(result => {
+    debug('createSignedUrl result: %j', result);
+    debug('Uploading to url: %s', result.put_url);
+    return uploadFile(result.put_url, contentLength, contentType);
+  });
+
+
+  function getUrls() {
+    return client.resources.storage.createSignedUrl();
+  }
+
+  function uploadFile(put_url, contentLength, contentType) {
+    let options = createRequestOptions(put_url, contentLength);
+    return new Promise((ok, nok) => {
+      const req = http.request(options, (res) => {
+        debug('Status: %d', res.statusCode);
+        debug('Headers: %j', res.headers);
+      });
+      req.on('error', (e) => {
+        debug('problem with request: %o', e.message);
+        nok(e);
+      });
+      const stream = toStream(body);
+      stream.pipe(req);
+      stream.on('end', () => {
+        debug('Streaming completed');
+        req.end();
+        ok();
+      });
+      if (!msg.attachments) {
+        msg.attachments = {};
+      }
+      msg.attachments[name] = {
+        url: put_url,
+        size: contentLength,
+        'content-type': contentType
+      };
+    });
+  }
+
+  function createRequestOptions(putUrl, contentLength) {
+    const opts = url.parse(putUrl);
+    opts.method = 'PUT';
+    opts.headers = {
+      'Content-Length': contentLength
+    };
+    return opts;
+  }
+}
+
+//const jsonata = require('@elastic.io/jsonata-moment');
+//const rp = require('request-promise');
 //
 //
 // const msg = {
@@ -74,14 +152,3 @@ const rp = require('request-promise');
 //       console.log("\n\n\n\n\n\n", requestBody);
 //     });
 
-const requestOptions = {
-  method: 'get',
-  uri: 'https://httpbin.org/image/png',
-  followRedirect: true,
-  followAllRedirects: true,
-  gzip: true,
-  resolveWithFullResponse: true,
-  simple: false
-};
-
-rp('https://httpbin.org/image/png',requestOptions).then(result=>{console.log(result)});
